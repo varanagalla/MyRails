5343c2d0cd4e75074127e52c864ee6d3bae6aed8
Vara Srinivasa <VSrinivasa@walmartlabs.com>
2013-09-04 07:54:10 -0700

    Finished exercises

--- test_sample_app/spec/requests/static_pages_spec.rb
+++ test_sample_app/spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   describe "Home page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -10,7 +10,7 @@ describe "Static pages" do
     end    
     it 'should have the right title | Home' do
       visit '/static_pages/home'
-      page.should have_selector('title',:text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title',:text => "#{base_title} | Home")
     end
   end
   
@@ -24,7 +24,7 @@ describe "Static pages" do
     it 'should have the right title | Help' do
       visit '/static_pages/help'
       page.should have_selector('title',
-                  :text => " | Help")
+                  :text => "#{base_title} | Help")
     end
 
   end
@@ -39,7 +39,22 @@ describe "Static pages" do
     it 'should have the right title | About Us' do
       visit '/static_pages/about'
       page.should have_selector('title',
-                  :text => " | About Us")
+                  :text => "#{base_title} | About Us")
+    end
+
+  end
+  
+  describe "Contact page" do
+    it "should have the content 'About Us'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+
+    it 'should have the right title | Contact' do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                  :text => " | Contact")
     end
 
   end
