f9888df2c397663fb4a89dc01d960d900fedce86
Vara Srinivasa <VSrinivasa@walmartlabs.com>
2013-09-03 21:34:14 -0700

    Finished demo app

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'afe02d6dc59551c105f3f017fefa7a615ada08b03183465d246da46964718546e391982b3137da7c81f1710cc1f0b0b855113a530791f9436054b90183b9661e'
