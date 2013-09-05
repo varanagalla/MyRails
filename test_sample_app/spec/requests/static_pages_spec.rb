bc20a486160ee63a9012aebe2ee3016bc583810e
Vara Srinivasa <VSrinivasa@walmartlabs.com>
2013-09-04 20:49:47 -0700

    Finished exercises for layout and routes

--- test_sample_app/spec/requests/static_pages_spec.rb
+++ test_sample_app/spec/requests/static_pages_spec.rb
@@ -1,14 +1,26 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  # let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   subject { page }
+  
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading)}
+    it { should have_selector('title', text: full_title(page_title))}
+  end
+  
   describe "Home page" do
     before { visit root_path }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: '| Home'}
+  end
+=begin
     it { should have_selector('h1', text: 'Sample App')}
     it { should have_selector('title', text: full_title(''))}
     it { should_not have_selector('title', text: " | Home")}
-   end
+=end
 =begin
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -28,9 +40,14 @@ describe "Static pages" do
   
   describe "Help page" do
     before { visit help_path }
+    let(:heading) { 'Sample App Help' }
+    let(:page_title) { 'Help' }
+    it_should_behave_like "all static pages"
+  end
+=begin
     it { should have_selector('h1', text: 'Sample App Help')}
     it { should have_selector('title', text: full_title('Help'))}
-  end
+=end
 =begin
     it "should have the content 'Sample App Help'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -47,9 +64,14 @@ describe "Static pages" do
    
   describe "About page" do
     before{ visit about_path }
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About Us' }
+    it_should_behave_like "all static pages"
+  end
+=begin
     it { should have_content('About Us') }
     it { should have_selector('title', text: full_title('About Us'))}
-  end
+=end
 =begin
     it "should have the content 'About Us'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -67,9 +89,14 @@ describe "Static pages" do
   
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+    it_should_behave_like "all static pages"
+  end
+=begin
     it { should have_content('Contact') }
     it { should have_selector('title', text: full_title('Contact'))}
-  end
+=end
 =begin
     it "should have the content 'About Us'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -83,6 +110,25 @@ describe "Static pages" do
                   :text => "#{base_title} | Contact")
     end
 =end
+
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      page.should have_selector 'title', text: full_title('About Us')
+      
+      click_link "Help"
+      page.should have_selector 'title', text: full_title('Help')
+      
+      click_link "Contact"
+      page.should have_selector 'title', text: full_title('Contact')
+      
+      click_link "Home"
+      click_link "Sign up now!"
+      page.should have_selector 'h1', text: 'Sign up'
+      
+      click_link "sample app"
+      page.should have_selector 'h1', text: 'Sample App'
+    end
  
   
   
