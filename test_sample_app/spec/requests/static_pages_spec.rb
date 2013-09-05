42bbdb85ca85849f9eeae071f84ddf6807ee2c32
Vara Srinivasa <VSrinivasa@walmartlabs.com>
2013-09-05 06:08:08 -0700

    Make a basic User Model (including secure passwords)

--- test_sample_app/spec/requests/static_pages_spec.rb
+++ test_sample_app/spec/requests/static_pages_spec.rb
@@ -114,20 +114,20 @@ describe "Static pages" do
     it "should have the right links on the layout" do
       visit root_path
       click_link "About"
-      page.should have_selector 'title', text: full_title('About Us')
+      should have_selector 'title', text: full_title('About Us')
       
       click_link "Help"
-      page.should have_selector 'title', text: full_title('Help')
+      should have_selector 'title', text: full_title('Help')
       
       click_link "Contact"
-      page.should have_selector 'title', text: full_title('Contact')
+      should have_selector 'title', text: full_title('Contact')
       
       click_link "Home"
       click_link "Sign up now!"
-      page.should have_selector 'h1', text: 'Sign up'
+      should have_selector 'h1', text: 'Sign up'
       
       click_link "sample app"
-      page.should have_selector 'h1', text: 'Sample App'
+      should have_selector 'h1', text: 'Sample App'
     end
  
   
